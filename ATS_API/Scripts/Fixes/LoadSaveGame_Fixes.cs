28d36452ba6d1282a428be18eff9cb47e38755ac
James <james.veugelaers@outlook.co.nz>
2024-05-01 00:37:24 +1200

    Fixed new fuelds breaking existing builds

---
+++ ATS_API/Scripts/Fixes/LoadSaveGame_Fixes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using ATS_API.Effects;
+using ATS_API.Goods;
+using Cysharp.Threading.Tasks;
+using Eremite.Controller.Generator;
+using Eremite.Model;
+using Eremite.Services;
+using HarmonyLib;
+
+namespace ATS_API.Fixes;
+
+[HarmonyPatch]
+internal class LoadSaveGame_Fixes
+{
+    [HarmonyPatch(typeof(GameLoader), nameof(GameLoader.LoadState))]
+    [HarmonyPostfix]
+    public static async UniTask PostLoadGame(UniTask enumerator, GameLoader __instance)
+    {
+        await enumerator;
+        foreach (NewGood effect in GoodsManager.NewGoods)
+        {
+            if (effect.goodModel.canBeBurned)
+            {
+                if (!__instance.state.prefs.fuelsPriority.ContainsKey(effect.goodModel.name))
+                {
+                    // Loaded a game that has new fuel types
+                    __instance.state.prefs.fuelsPriority.Add(effect.goodModel.name, 0); // ATS uses 0 as default
+                }
+            }
+        }
+    }
+}
