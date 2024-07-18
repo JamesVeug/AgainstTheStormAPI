773df3311ee31426fae2e88cf96e504d2d31046f
James <james.veugelaers@outlook.co.nz>
2024-07-18 18:47:37 +1200

    Added export audio clip support

--- ATS_API/Scripts/Helpers/AudioHelpers.cs
+++ ATS_API/Scripts/Helpers/AudioHelpers.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -110,4 +111,108 @@ public static class AudioHelpers
             }
         }
     }
+    
+    public static string ExportAudioClip(AudioClip clip, string path)
+    {
+        string directory = Path.GetDirectoryName(path);
+        if (!Directory.Exists(directory))
+        {
+            Directory.CreateDirectory(directory);
+        }
+        
+        using (var fileStream = CreateEmpty(path)) {
+
+	        ConvertAndWrite(fileStream, clip);
+
+	        WriteHeader(fileStream, clip);
+        }
+ 
+        return path;
+    }
+    
+    private static FileStream CreateEmpty(string filepath) {
+	    var fileStream = new FileStream(filepath, FileMode.Create);
+	    byte emptyByte = new byte();
+
+	    for(int i = 0; i < 44; i++) //preparing the header
+	    {
+		    fileStream.WriteByte(emptyByte);
+	    }
+
+	    return fileStream;
+    }
+    
+    private static void ConvertAndWrite(FileStream fileStream, AudioClip clip) {
+		var samples = new float[clip.samples];
+		clip.GetData(samples, 0);
+
+		Int16[] intData = new Int16[samples.Length];
+		//converting in 2 float[] steps to Int16[], //then Int16[] to Byte[]
+
+		Byte[] bytesData = new Byte[samples.Length * 2];
+		//bytesData array is twice the size of
+		//dataSource array because a float converted in Int16 is 2 bytes.
+
+		int rescaleFactor = 32767; //to convert float to Int16
+
+		for (int i = 0; i<samples.Length; i++) {
+			intData[i] = (short) (samples[i] * rescaleFactor);
+			Byte[] byteArr = new Byte[2];
+			byteArr = BitConverter.GetBytes(intData[i]);
+			byteArr.CopyTo(bytesData, i * 2);
+		}
+
+		fileStream.Write(bytesData, 0, bytesData.Length);
+	}
+
+	private static void WriteHeader(FileStream fileStream, AudioClip clip) {
+		var hz = clip.frequency;
+		var channels = clip.channels;
+		var samples = clip.samples;
+
+		fileStream.Seek(0, SeekOrigin.Begin);
+
+		Byte[] riff = System.Text.Encoding.UTF8.GetBytes("RIFF");
+		fileStream.Write(riff, 0, 4);
+
+		Byte[] chunkSize = BitConverter.GetBytes(fileStream.Length - 8);
+		fileStream.Write(chunkSize, 0, 4);
+
+		Byte[] wave = System.Text.Encoding.UTF8.GetBytes("WAVE");
+		fileStream.Write(wave, 0, 4);
+
+		Byte[] fmt = System.Text.Encoding.UTF8.GetBytes("fmt ");
+		fileStream.Write(fmt, 0, 4);
+
+		Byte[] subChunk1 = BitConverter.GetBytes(16);
+		fileStream.Write(subChunk1, 0, 4);
+
+		UInt16 two = 2;
+		UInt16 one = 1;
+
+		Byte[] audioFormat = BitConverter.GetBytes(one);
+		fileStream.Write(audioFormat, 0, 2);
+
+		Byte[] numChannels = BitConverter.GetBytes(channels);
+		fileStream.Write(numChannels, 0, 2);
+
+		Byte[] sampleRate = BitConverter.GetBytes(hz);
+		fileStream.Write(sampleRate, 0, 4);
+
+		Byte[] byteRate = BitConverter.GetBytes(hz * channels * 2); // sampleRate * bytesPerSample*number of channels, here 44100*2*2
+		fileStream.Write(byteRate, 0, 4);
+
+		UInt16 blockAlign = (ushort) (channels * 2);
+		fileStream.Write(BitConverter.GetBytes(blockAlign), 0, 2);
+
+		UInt16 bps = 16;
+		Byte[] bitsPerSample = BitConverter.GetBytes(bps);
+		fileStream.Write(bitsPerSample, 0, 2);
+
+		Byte[] datastring = System.Text.Encoding.UTF8.GetBytes("data");
+		fileStream.Write(datastring, 0, 4);
+
+		Byte[] subChunk2 = BitConverter.GetBytes(samples * channels * 2);
+		fileStream.Write(subChunk2, 0, 4);
+	}
 }
