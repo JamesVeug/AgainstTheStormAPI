c1f5b5d28d3138878dacd3f6ae42d3529d3da1bb
James <james.veugelaers@outlook.co.nz>
2024-11-05 21:43:07 +1300

    Save loading example (breaks atm)

--- ExampleMod/Plugin.cs
+++ ExampleMod/Plugin.cs
@@ -1,5 +1,5 @@
 ﻿using ATS_API.Helpers;
-using ATS_API.MetaRewards;
+using ATS_API.SaveLoading;
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
@@ -25,8 +25,9 @@ public partial class Plugin : BaseUnityPlugin
         Instance = this; 
         Log = Logger;
         harmony = Harmony.CreateAndPatchAll(typeof(Plugin).Assembly, PluginInfo.PLUGIN_GUID);
-
         PluginDirectory = Info.Location.Replace("API_ExampleMod.dll", "");
+        
+        ModdedSaveManager.ListenForLoadedSaveData(PluginInfo.PLUGIN_GUID, PostLoadedSaveData);
         
         CreateGoods();
         CreateCornerstones();
@@ -41,6 +42,22 @@ public partial class Plugin : BaseUnityPlugin
 
         CreateEmbarkRewards();
 
+
         Log.LogInfo($"{PluginInfo.PLUGIN_NAME} v{PluginInfo.PLUGIN_VERSION} Plugin loaded");
+    }
+
+    private void PostLoadedSaveData(ModSaveData saveData, SaveFileState saveState)
+    {
+        if (saveState == SaveFileState.NewFile)
+        {
+            Log.LogInfo($"New save file created for {PluginInfo.PLUGIN_NAME}");
+            saveData.General.SetValue("TotalLoads", 0);
+        }
+        else
+        {
+            int value = saveData.General.GetValueAsObject<int>("TotalLoads") + 1;
+            saveData.General.SetValue("TotalLoads", value);
+            Log.LogInfo($"Loaded save file for {PluginInfo.PLUGIN_NAME} {value} times");
+        }
     }
 }
