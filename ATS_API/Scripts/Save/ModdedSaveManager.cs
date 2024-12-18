65cc5bcc22b6773a4352a55404f59c2979f96352
Forwindz <1163710787@qq.com>
2024-12-18 23:21:42 +0100

    Add documentation

--- ATS_API/Scripts/Save/ModdedSaveManager.cs
+++ ATS_API/Scripts/Save/ModdedSaveManager.cs
@@ -1,22 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using ATS_API.Helpers;
 using Cysharp.Threading.Tasks;
 using Eremite;
-using UnityEngine;
 
 namespace ATS_API.SaveLoading;
 
 public static partial class ModdedSaveManager
 {
     internal static Dictionary<string, ModSaveData> ModGuidToDataLookup => m_ModSaveData;
-    
-    // %userprofile%\AppData\LocalLow\Eremite Games\Against the Storm\
+
+    /// <summary>
+    /// The folder path where mod save data is stored. 
+    /// The actual path varies based on the current profile being used.
+    /// 
+    /// By default, the path is: 
+    /// <c>%userprofile%\AppData\LocalLow\Eremite Games\Against the Storm\</c>
+    /// 
+    /// If you are using the experimental version, the base path is: 
+    /// <c>%userprofile%\AppData\LocalLow\Eremite Games\Against the Storm - Experimental\</c>
+    /// 
+    /// For the default profile, these paths represent the actual save folder locations. 
+    /// For other profiles, the save data is stored in the corresponding profile folder. 
+    /// In such cases, the folder path would look like: 
+    /// <c>%userprofile%\AppData\LocalLow\Eremite Games\Against the Storm {- Experimental}\{profile_id}\</c>
+    /// </summary>
     public static string PathToSaveFile => SO.ProfilesService.GetProfileFolderPath();
 
     private static readonly Dictionary<string, ModSaveData> m_ModSaveData = new Dictionary<string, ModSaveData>();
 
+    /// <summary>
+    /// Retrieves the mod save data based on the specified mod GUID.
+    /// If the <see cref="ModdedSaveManager"/> is not initialized and load data, 
+    /// this method will throw an exception.
+    /// </summary>
+    /// <param name="guid">The GUID of your mod.</param>
+    /// <returns>The <see cref="ModSaveData"/> associated with the specified GUID.</returns>
+    /// <exception cref="Exception">Thrown when <see cref="ModdedSaveManager"/> is not initialized.</exception>
     public static ModSaveData GetSaveData(string guid)
     {
         if (ModdedSaveManagerService.Instance == null || !ModdedSaveManagerService.Instance.Loaded)
@@ -33,16 +53,43 @@ public static partial class ModdedSaveManager
         return m_ModSaveData[guid];
     }
 
+    /// <summary>
+    /// If the <see cref="ModdedSaveManager"/> has any corresponding mod data
+    /// </summary>
+    /// <param name="guid">mod guid</param>
+    /// <returns></returns>
     public static bool ContainsSaveData(string guid)
     {
         return m_ModSaveData.ContainsKey(guid);
     }
 
+    /// <summary>
+    /// Save all data to the disk
+    /// </summary>
     public static void SaveAllModdedData()
     {
         ModdedSaveManagerService.Instance.SaveAllModdedData();
     }
 
+    /// <summary>
+    /// Adds a listener that will be invoked when the corresponding mod data is loaded. 
+    /// This typically occurs when the user switches profiles or launches the program for the first time.
+    /// </summary>
+    /// <param name="guid">The GUID of your mod.</param>
+    /// <param name="callback">
+    /// A callback function that receives the following parameters:
+    /// <see cref="ModSaveData"/> - Contains all the data for the corresponding mod.
+    /// <see cref="SaveFileState"/> - Indicates whether the save file is newly created or loaded from existing data.
+    /// <list type="bullet">
+    ///   <item>
+    ///     <see cref="SaveFileState.NewFile"/> - Indicates a new save file was created by the API. 
+    ///     Initialization operations may be required in this case.
+    ///   </item>
+    ///   <item>
+    ///     <see cref="SaveFileState.LoadedFile"/> - Indicates the data was loaded from an existing save file.
+    ///   </item>
+    /// </list>
+    /// </param>
     public static void ListenForLoadedSaveData(string guid, Action<ModSaveData, SaveFileState> callback)
     {
         if (!ModdedSaveManagerService.LoadedSaveDataListeners.TryGetValue(guid, out SafeAction<ModSaveData, SaveFileState> listeners))
