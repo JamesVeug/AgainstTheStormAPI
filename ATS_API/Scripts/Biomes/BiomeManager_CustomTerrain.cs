46d3ff7bdc1d31c27f270fd4bf0ea64f3b291990
James <james.veugelaers@outlook.co.nz>
2024-12-19 23:10:47 +1300

    Added custom rain support for biomes

--- ATS_API/Scripts/Biomes/BiomeManager_CustomTerrain.cs
+++ ATS_API/Scripts/Biomes/BiomeManager_CustomTerrain.cs
@@ -2,32 +2,58 @@
 using Cysharp.Threading.Tasks;
 using Eremite;
 using Eremite.Controller.Generator;
+using Eremite.View.Cameras;
 using HarmonyLib;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace ATS_API.Biomes;
 
 [HarmonyPatch]
 public static partial class BiomeManager
 {
-    [HarmonyPatch(typeof(MapGenerator), nameof(MapGenerator.SetTerrain))]
-    [HarmonyPostfix]
-    private static async UniTask InstantiateCustomTerrain(UniTask enumerator, MapGenerator __instance) 
+    [HarmonyPatch(typeof(MapGenerator), nameof(MapGenerator.SetTerrain), typeof(MapData))]
+    [HarmonyPrefix]
+    private static bool InstantiateCustomTerrain(MapData mapData, MapGenerator __instance, ref UniTask __result)
     {
+        // __result = UniTask.CompletedTask;
+        // return true;
+        // return;
         // This creates the terrain for the entire map. 
         // Contains FX for seasons, terrain, water, strider path, fog... etc
         // NOTE: The API does not support creating your own mapData.terrain at this stage. Just the terrain itself
-        await enumerator; // await mapData.terrain.InstantiateAsync();
-
         
         // Destroy the terrain from the model and put in our own
         BiomeTypes biomeTypes = __instance.conditions.biomeName.ToBiomeTypes();
         if (!NewBiomeLookup.TryGetValue(biomeTypes, out NewBiome newBiome))
         {
             // Plugin.Log.LogError($"{__instance.conditions.biomeName} is not a custom biome with type: {biomeTypes} with custom biomes: {NewBiomeLookup.Count}");
-            return;
+            // await enumerator; // await mapData.terrain.InstantiateAsync();
+            return true;
         }
 
+        
+        __result = InstantiateCustomTerrainAsync(mapData, __instance, newBiome);
+        return false;
+    }
+
+    private static async UniTask InstantiateCustomTerrainAsync(MapData mapData, MapGenerator __instance, NewBiome newBiome)
+    {
+        GameObject map = null;
+        var async = mapData.terrain.InstantiateAsync();
+        async.Completed += (asyncOperation) =>
+        {
+            Plugin.Log.LogInfo("Terrain instantiated!");
+            Plugin.Log.LogInfo("Terrain: " + asyncOperation.Result);
+            map = asyncOperation.Result;
+            CreateCustomTerrain(newBiome);
+            // ChangeRaindrops(newBiome, map);
+        };
+        await async;
+    }
+
+    private static void CreateCustomTerrain(NewBiome newBiome)
+    {
         if (newBiome.ACustomTerrain == null)
         {
             // No custom terrain for this biome. Will use default
@@ -40,7 +66,7 @@ public static partial class BiomeManager
         {
             // NOTE: Not all biomes have GameTerrain component on them which is where active terrain comes from.
             // Without it we can't find it... at this stage so at this point everything is broken
-            Plugin.Log.LogError("Terrain not found!");
+            Plugin.Log.LogError("Terrain not found to create custom terrain!");
             return;
         }
         
---
+++ ATS_API/Scripts/Biomes/BiomeManager_Rain.cs
@@ -0,0 +1,115 @@
+﻿using ATS_API.Helpers;
+using Cysharp.Threading.Tasks;
+using Eremite;
+using Eremite.Controller.Generator;
+using Eremite.Services;
+using Eremite.View.Cameras;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ATS_API.Biomes;
+
+[HarmonyPatch]
+public static partial class BiomeManager
+{
+    [HarmonyPatch(typeof(PostProcessesAnimator), nameof(PostProcessesAnimator.SetUp))]
+    [HarmonyPrefix]
+    private static bool PostProcessesAnimator_SetUp(PostProcessesAnimator __instance)
+    {
+        Plugin.Log.LogInfo("PostProcessesAnimator_SetUp: " + __instance.gameObject.FullName());
+        
+        BiomeTypes biomeTypes = Serviceable.StateService.Conditions.biomeName.ToBiomeTypes();
+        if (!NewBiomeLookup.TryGetValue(biomeTypes, out NewBiome newBiome))
+        {
+            Plugin.Log.LogError($"{Serviceable.StateService.Conditions.biomeName} is not a custom biome with type: {biomeTypes} with custom biomes: {NewBiomeLookup.Count}");
+            return true;
+        }
+        
+        ChangeRaindrops(newBiome, __instance.gameObject);
+        return true;
+    }
+
+    private static void ChangeRaindrops(NewBiome newBiome, GameObject fogAnimator)
+    {
+        if(newBiome.rainPrefab == null)
+        {
+            return;
+        }
+        
+        ParticleSystem rainDrops = fogAnimator.FindChild<ParticleSystem>("Rain");
+        if(rainDrops == null)
+        {
+            Plugin.Log.LogError("Rain particles not found!");
+        }
+        else
+        {
+            GameObject rain = GameObject.Instantiate(newBiome.rainPrefab, rainDrops.transform.parent);
+            rain.transform.localPosition = rainDrops.transform.localPosition;
+            rain.transform.localRotation = rainDrops.transform.localRotation;
+            rain.transform.localScale = rainDrops.transform.localScale;
+            rain.SetActive(true);
+            rain.name = rainDrops.gameObject.name;
+            GameObject.Destroy(rainDrops.gameObject);
+            Plugin.Log.LogInfo("Raindrops changed!");
+        }
+        
+        // Change Storm/Rain RainDrop
+        GameObject storm = fogAnimator.FindChild("Storm");
+        if(storm == null)
+        {
+            Plugin.Log.LogError("Raindrops storm not found!");
+        }
+        else
+        {
+            if (storm.TryGetComponent(out ParticleSystem particleSystem))
+            {
+                GameObject.Destroy(particleSystem);
+            }
+            
+            ParticleSystem stormRainDrop = storm.FindChild<ParticleSystem>("RainDrop");
+            if(stormRainDrop == null)
+            {
+                Plugin.Log.LogError("Storm/RainDrop storm not found!");
+            }
+            else
+            {
+                GameObject rain = GameObject.Instantiate(newBiome.rainPrefab, stormRainDrop.transform.parent);
+                rain.transform.localPosition = stormRainDrop.transform.localPosition;
+                rain.transform.localRotation = stormRainDrop.transform.localRotation;
+                rain.transform.localScale = stormRainDrop.transform.localScale;
+                rain.SetActive(true);
+                rain.name = stormRainDrop.gameObject.name;
+                GameObject.Destroy(stormRainDrop.gameObject);
+                Plugin.Log.LogInfo("Storm/RainDrop storm changed!");
+            }
+            
+            ParticleSystem stormRainDrop2 = storm.FindChild<ParticleSystem>("RainDrop2");
+            if(stormRainDrop2 == null)
+            {
+                Plugin.Log.LogError("Storm/RainDrop2 storm not found!");
+            }
+            else
+            {
+                GameObject rain = GameObject.Instantiate(newBiome.rainPrefab, stormRainDrop2.transform.parent);
+                rain.transform.localPosition = stormRainDrop2.transform.localPosition;
+                rain.transform.localRotation = stormRainDrop2.transform.localRotation;
+                rain.transform.localScale = stormRainDrop2.transform.localScale;
+                rain.SetActive(true);
+                rain.name = stormRainDrop2.gameObject.name;
+                GameObject.Destroy(stormRainDrop2.gameObject);
+                Plugin.Log.LogInfo("Storm/RainDrop2 storm changed!");
+            }
+        }
+
+        // Log the path of all childrens
+        // foreach (GameObject gameObject in SceneManager.GetActiveScene().GetRootGameObjects())
+        // {
+        //     ParticleSystem[] children = gameObject.GetComponentsInChildren<ParticleSystem>(true);
+        //     foreach (ParticleSystem child in children)
+        //     {
+        //         Plugin.Log.LogInfo("Child: " + child.FullName());
+        //     }
+        // }
+    }
+}
