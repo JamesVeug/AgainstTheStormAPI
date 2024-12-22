c542868b64946d218296904ad4f70209a69e0af5
James <james.veugelaers@outlook.co.nz>
2024-12-23 07:59:07 +1300

    Added config to turn on/off the DeveloperConsole

--- ATS_API/Scripts/Configs/Configs.cs
+++ ATS_API/Scripts/Configs/Configs.cs
@@ -6,6 +6,16 @@ namespace ATS_API;
 
 internal static class Configs
 {
+    public static bool DeveloperConsoleEnabled
+    {
+        get => m_DeveloperConsoleEnabled.Value;
+        set
+        {
+            m_DeveloperConsoleEnabled.Value = value;
+            Plugin.Instance.Config.Save();
+        }
+    }
+    
     public static bool ExportEnumTypes
     {
         get => m_ExportEnumTypes.Value;
@@ -25,6 +35,10 @@ internal static class Configs
             Plugin.Instance.Config.Save();
         }
     }
+    
+    private static ConfigEntry<bool> m_DeveloperConsoleEnabled = Bind("General", "Developer Console Enabled", false, 
+        "When set to true the QuantumConsole that the ATS developers use will be available to be used in the game via F1 by default.\n" +
+        "See KeyBindings tab in the games Options menu to changed the hotkey.");
 
     private static ConfigEntry<bool> m_ExportEnumTypes = Bind("Exporting", "Export Enum classes", false, 
         "When set to true the API will export all types of data as .cs files when the game opens for the API to use in the project.\n" +
---
+++ ATS_API/Scripts/DeveloperConsole/DeveloperConsole.cs
@@ -0,0 +1,80 @@
+﻿using Eremite;
+using QFSW.QC;
+using UnityEngine;
+
+namespace ATS_API.Scripts.DeveloperConsole
+{
+    internal static class DeveloperConsole
+    {
+        private static bool s_ConsoleEnabled = false;
+        
+        public static void Initialize()
+        {
+            Hotkeys.New(PluginInfo.PLUGIN_GUID, "toggleDevConsole", "Toggle Developer Console", [KeyCode.F1], ToggleConsole);
+        }
+
+        private static void ToggleConsole()
+        {
+            if (!Configs.DeveloperConsoleEnabled)
+            {
+                Plugin.Log.LogError("Developer console is disabled in the config");
+                return;
+            }
+            
+            if (!GameMB.IsGameActive)
+            {
+                Plugin.Log.LogError("Game is not active to toggle the console");
+                return;
+            }
+
+            QuantumConsole console = GetQuantumConsole();
+            if (console == null)
+            {
+                Plugin.Log.LogError("Unable to toggle the console");
+                return;
+            }
+            
+            s_ConsoleEnabled = !s_ConsoleEnabled;
+            
+            console.enabled = s_ConsoleEnabled;
+            console.GetComponent<Canvas>().enabled = s_ConsoleEnabled;
+            console.Toggle();
+            console.FocusConsoleInput();
+            if (s_ConsoleEnabled)
+            {
+                GameMB.TimeScaleService.Pause(false);
+                MB.InputService.LockInput(console);
+            }
+            else
+            {
+                GameMB.TimeScaleService.Unpause(false);
+                MB.InputService.ReleaseInput(console);
+            }
+            
+            Plugin.Log.LogMessage("Developer console toggled to " + (console.enabled ? "enabled" : "disabled"));
+        }
+
+        private static QuantumConsole s_cachedQuantumConsole = null;
+        private static QuantumConsole GetQuantumConsole()
+        {
+            if (s_cachedQuantumConsole == null)
+            {
+                s_cachedQuantumConsole = GameObject.FindObjectOfType<QuantumConsole>(true);
+                if(s_cachedQuantumConsole == null)
+                {
+                    Plugin.Log.LogError("QuantumConsole not found, creating new one");
+                    s_cachedQuantumConsole = new GameObject("QuantumConsole").AddComponent<QuantumConsole>();
+                }
+                else
+                {
+                    Plugin.Log.LogMessage("QuantumConsole found");
+                }
+
+                s_ConsoleEnabled = false;
+                s_cachedQuantumConsole.enabled = s_ConsoleEnabled;
+            }
+
+            return s_cachedQuantumConsole;
+        }
+    }
+}
