4953e8b4f64031922a4c013a3cf74b95ff418166
James <james.veugelaers@outlook.co.nz>
2024-12-25 23:27:50 +1300

    APILogger cleanup and more methods

--- ATS_API/APILogger.cs
+++ ATS_API/APILogger.cs
@@ -9,7 +9,7 @@ namespace ATS_API;
 /// Technically you can already do this with BepInEx by chaning what logs to console. But I find that tedious.
 /// If you want to use this in your mod then copy+paste this file into your mod and set logger on awake.
 ///
-/// LogDebug can be used everywhere since its unlikely people will have their log level set to Debug.
+/// LogDebug can be used everywhere since everyone starts set to Info
 /// </summary>
 internal static class APILogger
 {
@@ -99,58 +99,161 @@ internal static class APILogger
     {
         logger.LogError(exception + "\n" + GetStaceTraceIgnoreLogger());
     }
-    
-    public static void Assert(bool condition, string message)
-    {
-        if (!condition)
-        {
-            logger.LogError("Assertion failed: " + message);
-        }
-    }
-    
-    public static void Assert(bool condition, string message, params object[] args)
-    {
-        if (!condition)
-        {
-            logger.LogError("Assertion failed: " + string.Format(message, args));
-        }
-    }
 
     public static void IsTrue(bool condition, string message)
     {
-        Assert(condition, message);
-    }
-
-    public static void IsEqual<T>(T expected, T actual, string message)
-    {
-        string log = $"{expected} != {actual}, {message}";
-        if((expected == null) != (actual == null))
+        if (!condition)
         {
-            logger.LogError(log);
-            return;
+            logger.LogError(message);
         }
-        
-        Assert(expected.Equals(actual), log);
+    }
+    
+    public static void IsTrue(bool condition, string message, params object[] args)
+    {
+        if (!condition)
+        {
+            logger.LogError(string.Format(message, args));
+        }
     }
 
-    public static void IsNotEqual<T>(T expected, T actual, string message)
+    public static void IsFalse(bool condition, string message)
     {
-        string log = $"{expected} == {actual}, {message}";
-        if (expected == null)
+        if (condition)
         {
-            if (actual == null)
+            logger.LogError(message);
+        }
+    }
+    
+    public static void IsFalse(bool condition, string message, params object[] args)
+    {
+        if (condition)
+        {
+            logger.LogError(string.Format(message, args));
+        }
+    }
+    
+    public static void IsNull(object o, string message)
+    {
+        if (o != null)
+        {
+            logger.LogError(message);
+        }
+    }
+    
+    public static void IsNull(object o, string message, params object[] args)
+    {
+        if (o != null)
+        {
+            logger.LogError(string.Format(message, args));
+        }
+    }
+    
+    public static void IsNotNull(object o, string message)
+    {
+        if (o == null)
+        {
+            logger.LogError(message);
+        }
+    }
+    
+    public static void IsNotNull(object o, string message, params object[] args)
+    {
+        if (o == null)
+        {
+            logger.LogError(string.Format(message, args));
+        }
+    }
+
+    public static void IsEqual<T>(T a, T b, string message)
+    {
+        string log = $"{a} != {b}, {message}";
+        if (a == null)
+        {
+            if (b != null)
             {
                 logger.LogError(log);
             }
             return;
         }
-        else if (actual == null)
+        else if (b == null)
         {
-            // Not equal
+            // Equal
+            return;
+        }
+
+        if (!a.Equals(b))
+        {
+            logger.LogError(log);
+        }
+    }
+    
+    public static void IsEqual<T>(T a, T b, string message, params object[] args)
+    {
+        string log = $"{a} != {b}, {string.Format(message, args)}";
+        if (a == null)
+        {
+            if (b != null)
+            {
+                logger.LogError(log);
+            }
+            return;
+        }
+        else if (b == null)
+        {
+            // Equal
+            return;
+        }
+
+        if (!a.Equals(b))
+        {
+            logger.LogError(log);
+        }
+    }
+
+    public static void IsNotEqual<T>(T a, T b, string message)
+    {
+        string log = $"{a} == {b}, {message}";
+        if (a == null)
+        {
+            if (b == null)
+            {
+                logger.LogError(log);
+            }
+            return;
+        }
+        else if (b == null)
+        {
+            // Not Equal
             return;
         }
         
-        Assert(expected.Equals(actual), log);
+        if (a.Equals(b))
+        {
+            logger.LogError(log);
+        }
+    }
+    
+    public static void IsNotEqual<T>(T a, T b, string message, params object[] args)
+    {
+        string log = $"{a} == {b}, {string.Format(message, args)}";
+        if (a == null)
+        {
+            if (b == null)
+            {
+                logger.LogError(log);
+            }
+            return;
+        }
+        else if (b == null)
+        {
+            // Not Equal
+            return;
+        }
+        
+        if (a.Equals(b))
+        {
+            logger.LogError(log);
+        }
     }
 
     private static StackTrace GetStaceTraceIgnoreLogger()
